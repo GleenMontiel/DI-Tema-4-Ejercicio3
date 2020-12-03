52687d69458813aee48dc85ae95edee8007234c3
GleenMontiel <65775735+GleenMontiel@users.noreply.github.com>
2020-12-03 19:20:51 +0100

    First Commit

---
+++ Ejercicio3/Ejercicio3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    
+    public partial class Form1 : Form
+    {
+        public static Image imagen;
+        public static string tituloImagen;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro?", "Ejercicio3", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Image Files (*.jpg;*.png)|*.jpg;*.png|All files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+               
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        tituloImagen = Path.GetFileName(openFileDialog.FileName);
+                        imagen = Image.FromFile(openFileDialog.FileName);
+                        Form f2 = new Form2();
+                        if (checkBox1.Checked)
+                        {
+                            f2.ShowDialog();
+                        }
+                        else 
+                        {
+                            f2.Show();
+                        }
+                    }
+                    catch (System.OutOfMemoryException) 
+                    {
+                        Console.WriteLine("Error");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio3/Ejercicio3/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Form1.imagen;
+            this.Size = pictureBox1.Size;
+            this.Text = Form1.tituloImagen;
+        }
+
+        private void Form2_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Size = this.Size;
+        }
+    }
+}
